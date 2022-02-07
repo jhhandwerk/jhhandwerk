458432c2a5e55d27cf89e3fd8186c80df5fedb0e
Me <jlhofstra@yahoo.com>
2022-02-07 22:38:44 +0900

    added game 4, another 30 words

--- projects/portfolio/typing_games/LevelFour/app.js
+++ projects/portfolio/typing_games/LevelFour/app.js
@@ -2,15 +2,96 @@ const kanji = document.getElementById("kanji");
 const input = document.getElementById("input");
 //const value = input.value;
 const result = document.getElementById("result");
-const kanjiList = [
-"自身が能動的に考え行動します",
-"新し事にチャレンジし変化を創造します",
-"良い情報も、悪い情報も共有し組織改善に繋げます"]
-const yomiList = [
-"jishinnganoudoutekinikanngaekoudoushimasu", 
-"atarashikotonicyarennjishihennkawosouzoushimasu",
-"yoijyouhoumowaruijyouhoumokyouyushisoshikikaizennitsunagemasu"]
-const englishList = []
+const kanjiList = ["担当",
+"係員",
+"束",
+"腕",
+"抱える",
+"独立",
+"希望",
+"燃える",
+"耕す",
+"種",
+"草",
+"虫",
+"作物",
+"守る",
+"混乱",
+"肩",
+"抜く",
+"閉じる",
+"呼吸",
+"首相",
+"状況",
+"認める",
+"雨量",
+"湿度",
+"途中",
+"突然",
+"死亡",
+"深い",
+"沈む",
+"優しい",]
+const yomiList = ["tantou",
+"kakariinn",
+"taba",
+"ude",
+"kakeru",
+"dokuritsu",
+"kibou",
+"moeru",
+"tagayasu",
+"tane",
+"kusa",
+"mushi",
+"sakumotsu",
+"mamoru",
+"konran",
+"kata",
+"nuku",
+"tojiru",
+"kokyuu",
+"jyusyou",
+"jyoukyou",
+"mitomeru",
+"uryou",
+"shitsudo",
+"tocyuu",
+"totsuzen",
+"shibou",
+"fukai",
+"shizumu",
+"yasashii",]
+const englishList = ["responsible person",
+"Clerk",
+"bundle",
+"arm",
+"Hold",
+"independent",
+"hope",
+"burn",
+"Plow",
+"kind",
+"Grass",
+"insect",
+"crop",
+"protect",
+"confusion",
+"shoulder",
+"Pull out",
+"close up",
+"breathe",
+"Prime Minister",
+"Situation",
+"Admit",
+"rainfall",
+"humidity",
+"in the middle",
+"Sudden",
+"die",
+"deep",
+"Sink",
+"kind",]
 const sentenceList = []
 const vocabList = ["何 (what)<br>何 (what)","何 (what)<br>何 (what)","何 (what)<br>何 (what)","何 (what)<br>何 (what)","何 (what)<br>何 (what)"]
 const extraVocabList = [
@@ -98,6 +179,12 @@ input.addEventListener("keyup", function(event){
     english.innerHTML = ("&nbsp;");
     //sentence.innerHTML = (".");
     //extravocab.innerHTML = (".");
-
-
 });
+
+function twitterExample() {
+  window.open(`https://twitter.com/search?q=${kanjiList[randomNum]}&src=typed_query`, target='_blank')
+};
+
+function jishoExample() {
+  window.open(`https://jisho.org/search/${kanjiList[randomNum]}%20%23sentences`, target='_blank')
+};
